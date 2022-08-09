2acdd1e5675127fb544fab5c2046bddd8163835a
YashGodara <yash17godara@gmail.com>
2022-08-10 00:47:51 +0530

    changes

--- Java/Part7#Day5#06-08-22 XX/PractiseQ/q5INCOMPLETE.java
+++ Java/Part7#Day5#06-08-22 XX/PractiseQ/q5INCOMPLETE.java
@@ -2,13 +2,16 @@ package PractiseQ;
 
 import java.util.Scanner;
 
-public class q5 {
+public class q5INCOMPLETE {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter Year(for ex: 2022) ");
-        long a = sc.nextInt();
+        int yr = sc.nextInt();
 
+        
+        //Q- Write a Java program that takes a year from the user and print whether that year is a leap year or not.
+        
         
 
 
