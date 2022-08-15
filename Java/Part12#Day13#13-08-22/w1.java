4ab3142c3db606f11e253a0a9121a49c0dee8cc8
YashGodara <yash17godara@gmail.com>
2022-08-15 23:39:11 +0530

    Create w1.java

---
+++ Java/Part12#Day13#13-08-22/w1.java
@@ -0,0 +1,12 @@
+//Arrays -Introduction
+
+import java.util.*;
+
+public class w1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+    
+}
