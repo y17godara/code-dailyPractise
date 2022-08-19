a1f7bc25607fc1785ed8c9a37d222f57e2af275d
YashGodara <yash17godara@gmail.com>
2022-08-19 19:27:00 +0530

    Update q8.java

--- Java/Part12#Day13#14-08-22/q8.java
+++ Java/Part12#Day13#14-08-22/q8.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class q8 {
+
+    //Reverse of Array -Function
+
+    //Main
+    public static void main(String args[]){
+        
+        int nums[] = {2, 4, 6, 8, 10}; //array created
+
+    } 
     
 }
