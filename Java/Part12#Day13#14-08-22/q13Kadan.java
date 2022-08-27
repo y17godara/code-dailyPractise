46d290a6b69695a647e6462558b5b8966616c210
YashGodara <yash17godara@gmail.com>
2022-08-27 18:07:20 +0530

    questions

--- Java/Part12#Day13#14-08-22/q13Kadan.java
+++ Java/Part12#Day13#14-08-22/q13Kadan.java
@@ -1,3 +1,34 @@
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 public class q13Kadan {
+
+    //Prefix Function
+    public static void kadansArray(int nums[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0; //current sum
+
+        for(int i=0; i<nums.length; i++){
+
+            cs = cs+nums[i]; //new current sum changes with loop[i]
+
+            if(cs<0){ //if statement
+                cs=0;
+            }
+            ms= Math.max(cs, ms); //assign ms = maximum between cs and ms
+
+        }
+
+        System.out.println("Max Subarray Sum : "+ ms); //output
+    }
+
+    //Main
+    public static void main(String args[]){
+
+        int nums[] = {-2, -3, 4, -1, -2, 1, 5, -3}; //arrays defined
+
+        kadansArray(nums); //function called
+        //NOTE: If out Arrays Contains All the Maximum Value SO Kadans Algo Returns 0.
+        
+    }
     
 }
---
+++ Java/Part12#Day13#14-08-22/q14Rainwater.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class q14Rainwater {
+
+
+
+    //RainWater Function
+    public static void funWater(int barsH[]){
+
+        for(int i=0; i<barsH.length; i++){
+            
+        }
+
+
+       
+    }
+
+
+    //Main
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int barsH[] = {4, 2, 0, 6, 3, 2, 5}; //bars height -array assignes
+
+        funWater(barsH); //function Water -function called
+
+        sc.close();
+    }
+    
+}
