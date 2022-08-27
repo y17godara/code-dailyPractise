9d5804545f06d0bb9accea8a1d115d2c2e9c0d5c
YashGodara <yash17godara@gmail.com>
2022-08-28 01:41:00 +0530

    Update q14Rainwater.java

--- Java/Part12#Day13#14-08-22/q14Rainwater.java
+++ Java/Part12#Day13#14-08-22/q14Rainwater.java
@@ -1,21 +1,29 @@
+import java.nio.file.WatchEvent;
 import java.util.Scanner;
 
 public class q14Rainwater {
 
     //RainWater Function
-    public static void funWater(int barsH[]){
+    public static void funWater(int bars[]){
         int vol = 0;
+        int waterLevel = 0;
 
-        for(int i=0; i<barsH.length; i++){
 
-            vol = (barsH[i-1]-barsH[i])*1; // (water lvl/ bar lvl )*1
-            System.out.println("Vol is : ");
-            System.out.println(vol);
+        for(int i=0; i<bars.length; i++){
+
+            if(bars[i] <= bars[i+1]){
+
+                waterLevel = bars[i-1];
+
+            }else{
+
+                waterLevel = bars[i+1];
+            }
+
+            vol = (waterLevel - bars[i]) * 1;
+            System.out.println("Output : "+vol);
         }
 
-
-
-       
     }
 
 
@@ -23,10 +31,10 @@ public class q14Rainwater {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        int barsH[] = {4, 2, 0, 6, 3, 2, 5}; //bars height -array assignes
+        int bars[] = {4, 2, 0, 6, 3, 2, 5}; //bars height -array assignes
 
-        funWater(barsH); //function Water -function called
-
+        funWater(bars); //Condition
+   
         sc.close();
     }
     
