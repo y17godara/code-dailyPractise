46ad835a901d63fb00767d837081df0b3bc8c251
YashGodara <yash17godara@gmail.com>
2022-08-28 08:52:58 +0530

    Update q14Rainwater.java

--- Java/Part12#Day13#14-08-22/q14Rainwater.java
+++ Java/Part12#Day13#14-08-22/q14Rainwater.java
@@ -4,25 +4,12 @@ import java.util.Scanner;
 public class q14Rainwater {
 
     //RainWater Function
-    public static void funWater(int bars[]){
+    public static void funTrappedWater(int barHeight[]){
         int vol = 0;
         int waterLevel = 0;
 
 
-        for(int i=0; i<bars.length; i++){
-
-            if(bars[i] <= bars[i+1]){
-
-                waterLevel = bars[i-1];
-
-            }else{
-
-                waterLevel = bars[i+1];
-            }
-
-            vol = (waterLevel - bars[i]) * 1;
-            System.out.println("Output : "+vol);
-        }
+        
 
     }
 
@@ -31,9 +18,9 @@ public class q14Rainwater {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        int bars[] = {4, 2, 0, 6, 3, 2, 5}; //bars height -array assignes
+        int barHeight[] = {4, 2, 0, 6, 3, 2, 5}; //bars height -array assignes
 
-        funWater(bars); //Condition
+        funTrappedWater(barHeight); //Condition
    
         sc.close();
     }
