caa28f1683b04a44fcf761c96dfa9b2cdd335ec3
YashGodara <yash17godara@gmail.com>
2022-09-04 02:32:04 +0530

    Create insertionSort.java

---
+++ Java/Part13#Day15#16-08-22/insertionSort.java
@@ -0,0 +1,47 @@
+public class insertionSort {
+
+    public static void insertionSort(int arry[]) {
+
+        for(int i=1; i<arry.length; i++) {
+
+            int curr = arry[i]; //current element
+            int prev = i-1; //previous element
+
+            //finding out the correct position to insert element
+            while(prev >=0 && arry[prev] > curr) {
+
+                arry[prev + 1] = arry[prev];
+                prev--;
+            }
+            //inserting 
+            arry[prev + 1] = curr;
+
+        }
+    }
+
+    //Function -Array Printing
+    public static void printingArray(int arry[]) {
+
+        //Loop for Printing Array
+        for(int i=0; i<arry.length; i++) {
+
+            //Printing
+            System.out.print(arry[i]+" ");
+        }
+        System.out.println(); 
+    }
+
+
+    public static void main(String args[]) {
+        
+        int arry[] = {5, 4, 1, 3, 2}; //array defined
+
+        insertionSort(arry); //function calling
+
+        printingArray(arry); //printing array
+
+        /*Time Complexity = O(n^2) */
+     
+    }
+    
+}
