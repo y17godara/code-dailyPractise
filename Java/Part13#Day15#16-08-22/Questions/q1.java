3ee9f54c28873c930d5c6dff9de4d6ae57ed9f5f
Yash Godara <yash17godara@gmail.com>
2022-11-06 21:11:41 +0530

    updates

--- Java/Part13#Day15#16-08-22/Questions/q1.java
+++ Java/Part13#Day15#16-08-22/Questions/q1.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
 
 public class q1 {
 
     //Sort
     public static void bubbleSort(int arr[]){
 
-        for(int i=0; i<arr.lenght-1; i++){
-            for(int j=0; j<arr.lenght-1-i; j++){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
 
-                if(arr[i] > arr[j+1]){
+                if(arr[j] > arr[j+1]){
                     //swap
                     int temp = arr[j]; //temp container
                     arr[j] = arr[j+1];
@@ -16,21 +15,22 @@ public class q1 {
                 }
             }
         }
-
-    System.out.println(arr);
     }
 
+    //Function -Array Printing
+    public static void printingArray(int arry[]) {
+        for(int i=0; i<arry.length; i++) {
+            System.out.print(arry[i]+" ");
+        }
+        System.out.println();
+    }
 
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
 
-        //Question 1 [3, 6, 2, 1, 8, 7, 4, 5, 3, 1]
-        
-        int arr[] = [3, 6, 2, 1, 8, 7, 4, 5, 3, 1];
-
+        //Question 1 [3, 6, 2, 1, 8, 7, 4, 5, 3, 1] 
+        int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
         bubbleSort(arr);
 
-
-
+        printingArray(arr);
     }
 }
---
+++ Java/Part13#Day15#16-08-22/Questions/q2.java
@@ -0,0 +1,7 @@
+public class q2 {
+
+    public static void main(String args[]){
+        System.out.println("Hello");
+    }
+    
+}
