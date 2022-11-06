af383350b382b15e6bb4f790c5e01c9f332d0d84
Yash Godara <yash17godara@gmail.com>
2022-11-06 21:36:10 +0530

    Update q2.java

--- Java/Part13#Day15#16-08-22/Questions/q2.java
+++ Java/Part13#Day15#16-08-22/Questions/q2.java
@@ -1,7 +1,27 @@
 public class q2 {
 
+    //Selection
+    public static void selectionSort(int arr[]){
+
+        
+
+    }
+
+    //Function -Array Printing
+    public static void printingArray(int arry[]) {
+        for(int i=0; i<arry.length; i++) {
+            System.out.print(arry[i]+" ");
+        }
+        System.out.println();
+    }
+
     public static void main(String args[]){
-        System.out.println("Hello");
+
+        int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+        //call
+        selectionSort(arr);
+        printingArray(arr);
+
     }
     
 }
