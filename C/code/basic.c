dd36324ee5eccba6d4fb294d7ab07c284b19c001
Yash Godara <yash17godara@gmail.com>
2023-02-18 18:37:52 +0530

    C Added

---
+++ C/code/basic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void varOperSize(int start)
+{
+    //declaring 
+    int a;
+    char name;
+    float weight;
+    char gender;
+    const float PIE = 2.14;
+
+    printf("End");
+}
+
+
+//Main
+int main(void)
+{
+    printf("Hello,");
+    return 0;
+}
+
---
+++ C/code/rec.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(int num, int i)
+{
+    return num;
+
+}
+
+
+
+int main(void)
+{
+
+    int num, i;
+
+    num = 10; i = 50;
+
+
+    factorial(num, i);
+    
+    return 0;
+}
